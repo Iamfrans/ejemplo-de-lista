112f5c9d23cee5182892fc681679e3cf61aca26c
Iamfrans <1323544@senati.pe>
2023-03-04 09:54:21 -0500

    Mejora del codigo

--- lista.py
+++ lista.py
@@ -1,5 +1,5 @@
 colors = ['red', 'blue', 'brown', 'green', 'white']
 p1 = colors[0]
-p2 = colors[4]
+p2 = colors[len(colors) -1]
 print("el primer color de la lista es: ", (p1), " y ", "el ultimo color de la lista es: ", (p2) )
 
