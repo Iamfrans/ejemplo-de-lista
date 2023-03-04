77d320197dfcaded940a116e91caeacb44f3cddc
Iamfrans <1323544@senati.pe>
2023-03-04 09:58:15 -0500

    Agegand cmntario para 5 punos mas :v

--- lista.py
+++ lista.py
@@ -1,5 +1,7 @@
 colors = ['red', 'blue', 'brown', 'green', 'white']
 p1 = colors[0]
+#oro mtodo mas sensillo sin la funcion len[] 
+#p2 = colors[-1]
 p2 = colors[len(colors) -1]
 print("el primer color de la lista es: ", (p1), " y ", "el ultimo color de la lista es: ", (p2) )
 
