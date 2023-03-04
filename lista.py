5aeed60bc4fd9c1186272a4ea0b234fdffc1238f
Iamfrans <1323544@senati.pe>
2023-03-04 09:43:32 -0500

    Mejora de la interfaz

--- lista.py
+++ lista.py
@@ -1,5 +1,5 @@
 colors = ['red', 'blue', 'brown', 'green', 'white']
 p1 = colors[0]
 p2 = colors[4]
-print(p1)
-print(p2)
+print("el primer color de la lista es: ", (p1), " y ", "el ultimo color de la lista es: ", (p2) )
+
